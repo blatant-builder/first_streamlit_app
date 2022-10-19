dbffb9614d337b82bacaa404901b604637deda21
Blatant Builder <116133889+blatant-builder@users.noreply.github.com>
2022-10-18 23:18:04 -0500

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title("My Parent's New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 7 Blueberry Oatmeal")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-Bolied Free-Range Egg")
 
