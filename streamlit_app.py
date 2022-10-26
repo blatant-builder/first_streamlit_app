b7371f106abea2465fb740a643043e6e53a6efb5
Blatant Builder <116133889+blatant-builder@users.noreply.github.com>
2022-10-26 11:06:28 -0500

    Display fruityvice response as a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,10 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
+
+# Normalize the json version of the response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display the contents as a table
+streamlit.dataframe(fruityvice_normalized)
 
