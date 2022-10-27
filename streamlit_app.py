14aed6abf66d523f81be21f0ca3b8fc6b5d755e6
Blatant Builder <116133889+blatant-builder@users.noreply.github.com>
2022-10-26 22:33:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
+#streamlit.stop()
 
 # Allow user to add a fruit to the list
 def inert_row_snowflake(new_fruit):
